2e6e9c0b7bee91e2a48818dccc6f4a42bad022be
comradexlight <comradexlight@gmail.com>
2022-10-24 08:07:10 +0300

    from home to work

---
+++ src/js/app.js
@@ -0,0 +1,73 @@
+import data from '../data/data.json';
+
+class Table {
+	constructor(data) {
+    this.element = document.querySelector('table');
+    this.data = data;
+  }
+
+  addMainRow() {
+    const topicArray = ['id', 'title', 'year', 'imdb'];
+    const mainRow = document.createElement('tr');
+    for (const topic of topicArray) {
+      const cell = this.addCell(topic, 'th')
+      mainRow.appendChild(cell);
+    }
+    this.element.appendChild(mainRow);
+  }
+
+  addRow() {
+  	const row = document.createElement('tr');
+    row.classList.add('table_item')
+  	this.element.appendChild(row);
+  	return row
+  }
+
+  addCell(item, tag) {
+  	const cell = document.createElement(tag);
+  	cell.innerText = item;
+    cell.classList.add('table_item')
+  	return cell
+  }
+
+  sortData(key) {
+    this.data = this.data.sort((element1, element2) => element1[key] > element2[key] ? 1 : -1);
+  	this.removeTable();
+  	this.loadData();
+  }
+
+  removeTable() {
+  	const rows = document.querySelectorAll('.table_item');
+  	for (const row of rows) {
+  		row.remove();
+  	}
+  }
+
+ loadData() {
+    this.addMainRow();
+    for (const element of this.data) {
+      const row = this.addRow()
+      const cellId = this.addCell(element.id, 'td');
+      row.appendChild(cellId);
+      const cellTitle  = this.addCell(element.title, 'td');
+      row.appendChild(cellTitle);
+      const cellYearText = '(' + element.year + ')'
+      const cellYear  = this.addCell(cellYearText, 'td');
+      row.appendChild(cellYear);
+      const cellImdbtext = 'imdb: ' + Number(element.imdb).toFixed(2)
+      const cellImdb = this.addCell(cellImdbtext, 'td');
+      row.appendChild(cellImdb);
+    }
+  }
+}
+
+const table = new Table(data);
+table.loadData();
+
+const mainCell = document.querySelectorAll('th');
+for (const cell of mainCell) {
+  cell.addEventListener('click', function() {
+    table.sortData(cell.innerText);
+  })
+}
+
