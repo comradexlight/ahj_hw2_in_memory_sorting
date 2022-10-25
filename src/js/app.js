ebfe8fe4bb192545fcef9922d5ed72659d88e41d
comradexlight <comradexlight@gmail.com>
2022-10-25 17:30:35 +0300

    added sorted logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,62 +1,72 @@
-import data from '../data/data.json';
+import dataJson from '../data/data.json';
 
 class Table {
-	constructor(data) {
-    this.element = document.querySelector('table');
+  constructor(data) {
+    this.table = document.querySelector('table');
     this.data = data;
   }
 
   addRow() {
-  	const row = document.createElement('tr');
-    row.classList.add('table_item')
-  	this.element.appendChild(row);
-  	return row
+    const row = document.createElement('tr');
+    row.classList.add('table_item');
+    this.table.appendChild(row);
+    return row;
   }
 
-  addCell(item, tag) {
-  	const cell = document.createElement(tag);
-  	cell.innerText = item;
-    cell.classList.add('table_item')
-  	return cell
+  static addCell(element, tag) {
+    const cell = document.createElement(tag);
+    cell.innerText = element;
+    cell.classList.add('table_item');
+    return cell;
   }
 
-  sortData(key) {
-    this.data = this.data.sort((element1, element2) => element1[key] > element2[key] ? 1 : -1);
-  	this.removeTable();
-  	this.loadData();
+  sortData(cell) {
+    if (cell.classList.contains('sorted-down')) {
+      this.data = this.data.sort((e1, e2) => (e1[cell.innerText] > e2[cell.innerText] ? -1 : 1));
+      cell.classList.remove('sorted-down');
+      cell.classList.add('sorted-up');
+    } else {
+      this.data = this.data.sort((e1, e2) => (e1[cell.innerText] > e2[cell.innerText] ? 1 : -1));
+      cell.classList.add('sorted-down');
+      cell.classList.remove('sorted-up');
+    }
+    this.removeTable();
+    this.loadData();
   }
 
   removeTable() {
-  	const rows = document.querySelectorAll('.table_item');
-  	for (const row of rows) {
-  		row.remove();
-  	}
+    const rows = this.table.querySelectorAll('.table_item');
+    for (const row of rows) {
+      row.remove();
+    }
   }
 
- loadData() {
+  loadData() {
     for (const element of this.data) {
-      const row = this.addRow()
-      const cellId = this.addCell(element.id, 'td');
+      const row = this.addRow();
+      const cellId = Table.addCell(element.id, 'td');
       row.appendChild(cellId);
-      const cellTitle  = this.addCell(element.title, 'td');
+      const cellTitle = Table.addCell(element.title, 'td');
       row.appendChild(cellTitle);
-      const cellYearText = '(' + element.year + ')'
-      const cellYear  = this.addCell(cellYearText, 'td');
+      const cellYearText = `(${element.year})`;
+      const cellYear = Table.addCell(cellYearText, 'td');
       row.appendChild(cellYear);
-      const cellImdbtext = 'imdb: ' + Number(element.imdb).toFixed(2)
-      const cellImdb = this.addCell(cellImdbtext, 'td');
+      const cellImdbtext = `imdb: ${Number(element.imdb).toFixed(2)}`;
+      const cellImdb = Table.addCell(cellImdbtext, 'td');
       row.appendChild(cellImdb);
     }
   }
 
+  listenToClick() {
+    const mainRow = document.querySelectorAll('th');
+    for (const cell of mainRow) {
+      cell.addEventListener('click', () => {
+        this.sortData(cell);
+      });
+    }
+  }
 }
 
-const table = new Table(data);
+const table = new Table(dataJson);
 table.loadData();
-const mainCell = document.querySelectorAll('th');
-
-for (const element of mainCell) {
-  element.addEventListener('click', function() {
-    table.sortData(element.innerText);
-  })
-}
+table.listenToClick();
